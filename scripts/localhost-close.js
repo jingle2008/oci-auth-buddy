f5dd6e04cc8c9082df00aa96e09bd49eb72cf2ad
Jinguo Zhang <jinguo.zhang@oracle.com>
2025-06-22 09:35:14 -0700

    Robust localhost close: add polling fallback and observe document root for completion text

--- scripts/localhost-close.js
+++ scripts/localhost-close.js
@@ -1,9 +1,24 @@
 /**
  * Closes the window if "Authorization completed!" is detected on http://localhost:8181/*
  */
-(function () {
+
+function lookForCompletion() {
   if (/authorization completed!/i.test(document.body.innerText)) {
     logStep('Step8: Authorization completed, closing tab');
     chrome.runtime.sendMessage({ type: 'CLOSE_ME' });
+    return true;
   }
+  return false;
+}
+
+(function () {
+  let tries = 0;
+  const poll = setInterval(() => {
+    if (lookForCompletion()) {
+      clearInterval(poll);
+    } else if (++tries > 20) {
+      clearInterval(poll);
+      logStep('Step8: completion text not found in 5s');
+    }
+  }, 250);
 })();
